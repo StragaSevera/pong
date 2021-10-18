575755a99287c03512184db56e17916ba164ca76
StragaSevera <ought@yandex.ru>
2021-10-19 00:54:46 +0300

    Initial commit

---
+++ src/pong.rs
@@ -0,0 +1,28 @@
+use amethyst::{
+    core::Transform,
+    prelude::*
+};
+use amethyst::renderer::Camera;
+
+pub const ARENA_HEIGHT: f32 = 100.0;
+pub const ARENA_WIDTH: f32 = 100.0;
+
+pub struct Pong;
+
+impl SimpleState for Pong {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+        initialise_camera(world);
+    }
+}
+
+fn initialise_camera(world: &mut World) {
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(ARENA_WIDTH * 0.5, ARENA_HEIGHT * 0.5, 1.0);
+
+    world
+        .create_entity()
+        .with(Camera::standard_2d(ARENA_WIDTH, ARENA_HEIGHT))
+        .with(transform)
+        .build();
+}
